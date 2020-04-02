f50b2caddd50932534eb33efaab67d23ead8f99f
LightMan <39643591+LightMan01@users.noreply.github.com>
2020-04-02 10:26:25 +0300

    Encoding set

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,7 @@
 <?php
 
   $db = mysqli_connect('127.0.0.1', 'root', '', 'signupin');
+  mysqli_set_charset($db, "utf8");
 
   if (!$db) {
     echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
